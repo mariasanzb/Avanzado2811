f6b9c538a4623cd647777158c349a7079fd7238a
mariasanzb <142232678+mariasanzb@users.noreply.github.com>
2024-11-28 10:37:25 -0300

    no necesitaba surprise lol

--- streamlit/app_mery_svd.py
+++ streamlit/app_mery_svd.py
@@ -8,15 +8,9 @@ import gdown
 url = 'https://drive.google.com/uc?export=download&id=1tY2U8IjcUo2NTbXMWvY1Bg9eG8qn9fcK'
 gdown.download(url, 'reviews.parquet', quiet=False)
 
-# Ahora puedes cargar el archivo como un DataFrame de pandas o usarlo como lo necesites
-import pandas as pd
-reviews = pd.read_parquet('reviews.parquet',engine='pyarrow')
-
+# Cargar los archivos Parquet con pandas
+reviews = pd.read_parquet('reviews.parquet', engine='pyarrow')
 restaurant = pd.read_parquet('mi_archivo1.parquet', engine='pyarrow')
-
-# Crear el dataset de Surprise
-reader = Reader(rating_scale=(1, 5))
-data = Dataset.load_from_df(reviews[['user_id', 'business_id', 'rating']], reader)
 
 # Funciones auxiliares
 def get_restaurant_id(restaurant_title, metadata):
@@ -51,33 +45,3 @@ def generate_top_recommendations(user_id, model, metadata, top_n=5, thresh=4):
             restaurant_info = get_restaurant_info(restaurant_id, metadata)[0]
             restaurant_info['predicted_rating'] = rating
             recommendations.append(restaurant_info)
-    
-    recommendations = sorted(recommendations, key=lambda x: x['predicted_rating'], reverse=True)
-    return recommendations[:top_n]
-
-# Cargar el modelo serializado
-with open('./Data/svd_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-
-# Configurar la interfaz de Streamlit
-st.title("Sistema de Recomendación de Restaurantes")
-st.write("Obtén las mejores recomendaciones personalizadas para tus restaurantes favoritos.")
-
-user_id = st.text_input("Introduce el ID del usuario:")
-if user_id:
-    st.write(f"Mostrando recomendaciones para el usuario: {user_id}")
-    
-    top_recommendations = generate_top_recommendations(user_id, model, restaurant, top_n=5)
-    
-    if top_recommendations:
-        st.write("Top 5 restaurantes recomendados:")
-        for i, recommendation in enumerate(top_recommendations, start=1):
-            st.subheader(f"{i}. {recommendation['name']}")
-            st.write(f"- ID: {recommendation['business_id']}")
-            st.write(f"- Rating Predicho: {recommendation['predicted_rating']:.2f}")
-            st.write(f"- Estrellas: {recommendation['stars']}")
-            st.write(f"- Ubicación: {recommendation['latitude']}, {recommendation['longitude']}")
-            st.write("---")
-    else:
-        st.write("No se encontraron recomendaciones para este usuario.")
-
